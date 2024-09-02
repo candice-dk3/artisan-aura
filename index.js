669272484144bf261c7bf6c6baa0d70c71d4ed7f
candice-dk3 <candicekeet3@gmail.com>
2024-09-02 16:23:38 +0200

    Delete User works

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let port = process.env.PORT || 1003
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:1003',
+    origin: '*',
     Credentials:true
 }))
 
